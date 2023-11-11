c8d3a16b5e299e1bd39adb7eb713037b0a0fbcb5
Jeremias Gauto <jeremiasg_kc@hotmail.com>
2023-11-10 21:12:58 -0300

    Trayendo datos ok

--- src/components/Container.jsx
+++ src/components/Container.jsx
@@ -1,21 +1,79 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import MovieCard from './MovieCard'
 
 function Container() {
-  return (
-      <main className="container content">
-      <MovieCard />
-      <MovieCard />
-      <MovieCard />
-      <MovieCard />
-      <MovieCard />
-      <MovieCard />
-      <MovieCard />
-      <MovieCard />
-      <MovieCard />
-      
-    </main>
-  )
+
+    const [movies, setMovies] = useState([]) // useState retorna un arreglo
+    const [loader, setLoader] = useState(false)
+    const [page, setPage] = useState(1)
+
+
+
+    const getMovies = async () => {
+        try {
+
+            // https://api.themoviedb.org/3/movie/popular
+            const url = `${import.meta.env.VITE_URL}?language=es-ES&page=${page}` // template string 
+
+           
+
+            const config = {
+                method: "GET", // GET POR DEFECTO  METHODS = GET,POST,PUT,DELETE,PATCH,OPTIONS
+                headers: {
+                    'content-type': 'application/json',
+                    'authorization': `Bearer ${import.meta.env.VITE_TOKEN}`
+                },
+                //body:"" // para metodos PUT PATCH POST
+            }
+            const respuesta = await fetch(url, config)
+
+          if (respuesta.status === 200) {
+              console.log(respuesta.status)
+              const datos = await respuesta.json()
+              console.log(datos)
+                if (movies.length === 0) {
+                    setMovies(datos.results)
+                    console.log(datos.results)
+                } else {
+                    setMovies([movies, ...datos.results])
+                }
+
+            }
+
+
+        } catch (err) {
+            console.log(err.message)
+        }
+    }
+
+    const handleClick = (event) => {
+        event.preventDefault()
+        setPage(page + 1)
+    }
+
+    useEffect(() => {
+        getMovies()
+    }, [page]) // array de dependencias. si esta vacio se ejecuta una vez se monta el componente
+    // si tiene dependencias, se va a ejecutar el USEEFFECT cada vez que cambie la/s dependencia/s
+
+
+
+    return (
+        <>
+            <main className="container content" id='mimain'>
+                {/* {1 === '1' ? (loader ? <p>Cargando</p> : <p>No cargando</p>) : "no es igual a 1"} */}
+                {
+                    movies?.map((pelicula) => {
+                        return <MovieCard key={pelicula.id} pelicula={pelicula} />
+                    })
+                }
+
+            </main>
+            <button className='btn-seemore' onClick={handleClick}>Ver más</button>
+
+            {loader && <p>Cargando...</p>}
+        </>
+    )
 }
 
 export default Container
--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,18 +1,23 @@
 import React from 'react'
 import portada from "../assets/portada.jpg"
 
-function MovieCard() {
-  return (
-      <article className="moviecard">
-          <h3>Titulo Pelicula</h3>
-          <div className="moviecard-image">
-              
-              <img src={portada} alt="" />
-          
-          </div>
-          <p>Descripcion</p>
-      </article>
-  )
+
+function MovieCard ({ pelicula })// children key
+{ //LA FUNCION de COMPONENTES NO PUEDE SER ASINCRONA 
+    const { title, overview, poster_path, vote_averange, vote_count, release_date } = pelicula
+    return (
+        <article className='moviecard'>
+            <div className="moviecard-image">
+                <img src={`https://image.tmdb.org/t/p/w500${poster_path}`} alt={`Poster ${title}`} />
+            </div>
+            <h3>{title}</h3>
+
+            <p>
+                {overview}
+            </p>
+        </article>
+    )
 }
+
 
 export default MovieCard
